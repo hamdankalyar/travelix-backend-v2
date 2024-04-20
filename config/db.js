b6728aa2debc95b73893fba2fdebf5a963cfb96d
Hamdan <hamdan.kalyar@outlook.com>
2024-04-20 20:10:24 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 async function connectDb() {
   try {
     await mongoose.connect(
-      "mongodb+srv://hamdankalyar:kalyar@cluster0.xifgntj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-      //"mongodb+srv://hamdankalyar:kalyar@cluster0.r8gi2rk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      // "mongodb+srv://hamdankalyar:kalyar@cluster0.xifgntj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://hamdankalyar:kalyar@cluster0.r8gi2rk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Connected to MongoDB..");
   } catch (err) {
