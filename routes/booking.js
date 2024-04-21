6eca154307e42bce28e92a5f2375f414387c59ab
Hamdan <hamdan.kalyar@outlook.com>
2024-04-21 13:26:17 +0500

    Update booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -49,8 +49,8 @@ router.get("/", async (req, res) => {
 //gpt last code 
 router.post("/mobile", async (req, res) => {
   try {
-    const { error } = validateBooking(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    
+    
 
     const { bookedItem, paymentMethod } = req.body;
 
